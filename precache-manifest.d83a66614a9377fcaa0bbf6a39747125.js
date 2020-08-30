59ba37576e39648b55a0a9ee114209def12777d6
Erincz Arnold <arnold.erincz@gmail.com>
2020-08-30 13:11:19 +0300

    Updates

---
+++ precache-manifest.d83a66614a9377fcaa0bbf6a39747125.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f50a6a229c6fa40fa8db602766ec213",
+    "url": "/robo_friends/index.html"
+  },
+  {
+    "revision": "5e0bfb1b15836512c2e6",
+    "url": "/robo_friends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "762d7de5e3b33e806643",
+    "url": "/robo_friends/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5e0bfb1b15836512c2e6",
+    "url": "/robo_friends/static/js/2.68960da7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robo_friends/static/js/2.68960da7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "762d7de5e3b33e806643",
+    "url": "/robo_friends/static/js/main.42181e9f.chunk.js"
+  },
+  {
+    "revision": "cfaac14a898bebd2b6ce",
+    "url": "/robo_friends/static/js/runtime-main.2d916b2e.js"
+  }
+]);
