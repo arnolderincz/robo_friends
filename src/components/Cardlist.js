f53a528a1fb15a5a9f91ad2bd41d27fd5afc3312
Arnold Erincz <68700184+arnolderincz@users.noreply.github.com>
2020-09-07 21:18:56 +0300

    Update Cardlist.js

--- src/components/Cardlist.js
+++ src/components/Cardlist.js
@@ -2,11 +2,8 @@ import React from 'react';
 import Card from './Card';
 
 const Cardlist = ({robots}) => {
-   /* if(true){
-        throw new Error("nooooo");
-    }*/
     const cardComp = robots.map((user, i) => {
-        return <Card key={robots[i].id} id={robots[i].id} name={robots[i].name} email={robots[i].email}/>;
+        return <Card key={robots[i].id} id={robots[i].id} name={robots[i].name} email={robots[i].email}/>
     });
     return (
         <div>
